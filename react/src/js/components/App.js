62c92cbb8bc46b2013f43ac9bbd8a660daef5eb0
jeanpier <monserrateluisje@gmail.com>
2019-08-08 23:37:37 -0400

    nada nuevo xd

--- react/src/js/components/App.js
+++ react/src/js/components/App.js
@@ -1,9 +1,23 @@
-import React from "react"
+import React, {useState} from "react"
+import data from "./data.json"
+import Loaders from "./Loaders"
 
 function App(props){
+    const [loaderState, setLoaderState] = useState([])
+
+    function handleClick(){
+        setLoaderState(data.loaders)
+    }
+
     return (
         <div>
             <h1>Hola soy jeanpi</h1>
+            <ul>
+                {
+                    loaderState.map(item => <Loaders {...item} key={item.id} />)
+                }
+            </ul>
+            <button onClick={handleClick} >presiona aquí</button>
         </div>
     )
 }
