7c0d0cfdccf2c855e98730ee85b99312ddbacf39
jeanpier <monserrateluisje@gmail.com>
2019-08-09 18:00:31 -0400

    preprocesadores de estilos con webpack

---
+++ prepro/src/js/index.js
@@ -0,0 +1,10 @@
+import "../css/index.css"
+
+import React from "react"
+import { render } from "react-dom"
+import App from "./components/App"
+
+const container = document.getElementById("container")
+
+render (<App />, container)
+
