24aac1d98fbbf675e348229a56229966f22e57f4
jeanpier <monserrateluisje@gmail.com>
2019-08-08 22:53:53 -0400

    instalando react en el proyecto

---
+++ react/src/js/components/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function App(props){
+    return (
+        <div>
+            <h1>Hola soy jeanpi</h1>
+        </div>
+    )
+}
+
+export default App
---
+++ react/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require("webpack")
+
+module.exports = {
+    entry: {
+    home: path.resolve(__dirname, "src/js/index.js")
+    },
+    mode: "development",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "js/[name].js"
+    },
+    devServer: {
+        hot: true,
+        open: true,
+        port: 9000,
+    },
+    module: {
+        noParse: /jquery|lodash/,
+        rules: [
+        {
+                test: /\.js$/,
+                use: [
+                  "babel-loader"
+                     ]
+        },
+        {
+          test: /\.css$/,
+          use: [
+              "style-loader",
+              "css-loader"
+               ]
+        }
+     ]
+   },
+   plugins: [
+     new webpack.HotModuleReplacementPlugin(),
+     new HtmlWebpackPlugin({
+         title: "webpack-dev-server",
+         template:  path.resolve(__dirname, "index.html")
+     })
+   ]
+}
