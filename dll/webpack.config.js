4d71c2d7b5b2c2122dc33751bb9c2c93770c5de8
jeanpier <monserrateluisje@gmail.com>
2019-08-13 22:15:16 -0400

    usando un DLL

---
+++ dll/webpack.config.js
@@ -0,0 +1,98 @@
+const path = require("path")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require("webpack")
+
+module.exports = {
+    entry: {
+    home: path.resolve(__dirname, "src/js/index.js"),
+    contact: path.resolve(__dirname, "src/js/contact.js")
+    },
+    mode: "production",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "js/[name].js"
+    },
+    module: {
+        noParse: /jquery|lodash/,
+        rules: [
+        {
+                test: /\.js$/,
+                use: [
+                  "babel-loader"
+                     ],
+                     exclude: /node_modules/,
+        },
+        {
+          test: /\.css$/,
+          use: [
+              {
+                loader: MiniCssExtractPlugin.loader
+              },
+              {
+                loader:"css-loader",
+                options: {
+                  importLoaders: 1
+                }
+              },
+              "postcss-loader"
+               ]
+        },
+        {
+          test: /\.jpg|png|gif|svg|mp4|webm|woff|eot|ttf$/,
+          use: [{
+            loader:"url-loader",
+            options: {
+              limit: 90000
+            }
+          }
+               ],
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.less$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader
+            },
+              "css-loader",
+              "less-loader"
+               ]
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader
+            },
+              "css-loader",
+              "sass-loader"
+               ]
+        },
+        {
+          test: /\.styl$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader
+            },
+              "css-loader",
+              "stylus-loader"
+               ]
+        }
+     ]
+   },
+   plugins: [
+     new MiniCssExtractPlugin({
+       filename: "css/[name].css",
+       chunkFilename: "css/[id].css"
+     }),
+     new webpack.HotModuleReplacementPlugin(),
+     new HtmlWebpackPlugin({
+         title: "webpack-dev-server",
+         template:  path.resolve(__dirname, "index.html")
+     }),
+     new webpack.DllReferencePlugin({
+       manifest: require("./modules-manifest.json")
+     })
+   ]
+}
