61b53f5ae0fb1e294f003589607d8a49c75f9640
jeanpier <monserrateluisje@gmail.com>
2019-08-16 21:36:25 -0400

    arreglando bugs 1

--- platzi-badges/webpack.dev.config.js
+++ platzi-badges/webpack.dev.config.js
@@ -1,6 +1,6 @@
-const path = require("path"),
+const path = require("path")
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-
+const webpack = require("webpack")
 
 module.exports = {
     entry: {
@@ -44,7 +44,7 @@ module.exports = {
             }
           }
                ],
-          exclude: /node_modules/,
+         
         },
         
      ]
